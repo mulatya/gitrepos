bd1548f5cce81c822a63ce60af3d69111d4569ed
mulatya <emmanuelmulatya8@gmail.com>
2017-06-30 14:32:34 +0300

    first commit

---
+++ js/github.js
@@ -0,0 +1,14 @@
+var apiKey = require('./../.env').apiKey;
+
+github = function(){
+}
+
+github.prototype.getusername = function() {
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response) {
+    console.log(response)
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+}
+
+exports.githubModule = github;
